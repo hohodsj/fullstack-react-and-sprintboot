71d6446d72e05f802950d65a4b0d9de2a897c6cb
hohodsj <hohodsj@gmail.com>
2024-10-14 23:15:58 -0400

    type script basic

---
+++ section5-typescript-crash-course/02-variables/sample-types.js
@@ -0,0 +1,13 @@
+var found = true;
+var grade = 88.6;
+var firstName = "Anup";
+var lastName = 'Kumar';
+// lets break it
+// found = 0;
+// grade = "A";
+// firstName = false;
+console.log(found);
+console.log("The grade is " + grade);
+console.log("Hi " + firstName + " " + lastName);
+// use template
+console.log("Hi ".concat(firstName, " ").concat(lastName));
