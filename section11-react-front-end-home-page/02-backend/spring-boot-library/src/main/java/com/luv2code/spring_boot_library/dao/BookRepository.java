a5f4d3654f21b2a4c24c4def380fda9eb454c5af
hohodsj <hohodsj@gmail.com>
2025-02-19 23:16:02 -0500

    connect frontend with backend

--- section11-react-front-end-home-page/02-backend/spring-boot-library/src/main/java/com/luv2code/spring_boot_library/dao/BookRepository.java
+++ section11-react-front-end-home-page/02-backend/spring-boot-library/src/main/java/com/luv2code/spring_boot_library/dao/BookRepository.java
@@ -1,9 +1,11 @@
 package com.luv2code.spring_boot_library.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.luv2code.spring_boot_library.entity.Book;
 
+// @CrossOrigin("http://localhost:3000")
 public interface BookRepository extends JpaRepository<Book, Long> {
     
 }
